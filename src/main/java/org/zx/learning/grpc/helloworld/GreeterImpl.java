c1c9b7036252b08481c54fc313d2c55cdee44424
zouxiang <zouxiang@kuaishou.com>
2024-10-17 16:02:57 +0800

    grpc helloworld

---
+++ src/main/java/org/zx/learning/grpc/helloworld/GreeterImpl.java
@@ -0,0 +1,15 @@
+package org.zx.learning.grpc.helloworld;
+
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.stub.StreamObserver;
+
+public class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello, " + request.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
